9de67f25e48fa3f84b1d1f262e27d9956aaba238
Fynn <enriquefynn@gmail.com>
2017-11-26 18:04:53 +0100

    hash partitioning new file

---
+++ libpartitioning/include/hash_partitioning.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+
+#include <config.h>
+#include <partitioner.h>
+
+class Hash_partitioner : public Partitioner {
+
+public:
+  Hash_partitioner(const Graph &graph, const Config &config)
+      : Partitioner(0, graph, config) {}
+
+  void assign_partition(std::vector<int32_t> &partitioning,
+                        uint32_t from_vertex, uint32_t to_vertex,
+                        int32_t nparts);
+
+  std::vector<int32_t> partition(int32_t nparts) {
+    return std::vector<int32_t>();
+  };
+
+  bool trigger_partitioning(uint32_t new_timestamp,
+                            bool last_edge_cross_partition) {
+    return false;
+  };
+
+  Hash_partitioner &operator=(const Hash_partitioner &) = delete;
+  Hash_partitioner(const Hash_partitioner &) = delete;
+};
